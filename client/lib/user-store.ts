08414415ef817eae9c805fafacd448061b4b8867
AlejandroDalzotto <aledalzotto15@gmail.com>
2026-09-07 19:47:47 -0300

    adding reset method for deleting in-memory user on logout

--- client/lib/user-store.ts
+++ client/lib/user-store.ts
@@ -8,9 +8,11 @@ type UserState = {
     avatarUrl: string | null;
   } | null;
   setUser: (user: { name: string; avatarUrl: string | null }) => void;
+  reset: VoidFunction;
 };
 
 export const useUserStore = create<UserState>((set) => ({
   user: null,
   setUser: (user) => set({ user }),
+  reset: () => set({ user: null }),
 }));
