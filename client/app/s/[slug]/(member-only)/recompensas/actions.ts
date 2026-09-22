b7c871c4d7f96c04847aa1d87718ed6d3ab2d523
AlejandroDalzotto <aledalzotto15@gmail.com>
2026-09-22 04:05:19 -0300

    fixing various troblesome code.

--- client/app/s/[slug]/(member-only)/recompensas/actions.ts
+++ client/app/s/[slug]/(member-only)/recompensas/actions.ts
@@ -3,6 +3,7 @@
 import { request } from "@/lib/api";
 import { Reward } from "@/lib/types/reward";
 
-export async function getRewards(storefrontId: string, page: number, size: number = 10) {
+// TODO: should implement pagination and also send page: number, size: number = 10
+export async function getRewards(storefrontId: string) {
   return await request<Reward[]>(`/rewards?${new URLSearchParams(storefrontId)}`);
 }
